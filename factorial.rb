bc96aef1774b80f3e498923093c7d62e55755214
essilfiequansah <bquansah007@gmail.com>
2022-10-07 13:53:02 +0000

    add comments to code

--- factorial.rb
+++ factorial.rb
@@ -5,9 +5,11 @@ no_of_ns = gets.chomp.to_i
 no_of_ns.times do
     n = gets.chomp.to_i
 
+    # Initilize count 0
     count = 0
     
     if n >= 5
+#  Keep dividing n by 5 & update Count
         while n >= 5
             n /= 5
             count += n
